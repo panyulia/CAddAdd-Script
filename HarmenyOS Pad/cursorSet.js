4dbbd3df16b36f8bc0a322b2c29d624a72f964b5
panyuliang <98296889+panyulia@users.noreply.github.com>
2022-07-02 10:11:38 +0800

    HarmenyOS Pad

---
+++ HarmenyOS Pad/cursorSet.js
@@ -0,0 +1,21 @@
+/*
+isObject at cursor name,a your cursor file.
+default/*.svg = HarmenyOS Pad/cursors/(default/*.svg')
+*/
+window.harmenyOS=new Object({
+	array:"default/array.svg",
+	cross:"default/crosshair.svg",
+	help:"default/help.svg",
+	move:"default/move.svg",
+	no:"default/no.svg",
+	pen:"default/pen.svg",
+	hand:"default/pointer.svg",
+	sizeNESW:"default/sizeNESW.svg",
+	sizeNS:"default/sizeNS.svg",
+	sizeNWSE:"default/sizeNWSE.svg",
+	sizeWE:"default/sizeWE.svg",
+	text:"default/text.svg",
+	upArray:"default/upArray.svg",
+	wait:"default/wait.svg",
+	working:"default/working.svg"
+});
